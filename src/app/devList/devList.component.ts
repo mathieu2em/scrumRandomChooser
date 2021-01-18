762de5d853300ebbdd90541c775ad17505f84c03
Alexandre Chartrand <chartrandalexandre@gmail.com>
2021-01-18 01:51:07 -0500

    Drag and drop with Cart for DevList page.

--- src/app/devList/devList.component.ts
+++ src/app/devList/devList.component.ts
@@ -1,80 +1,106 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import {
-  CdkDragDrop,
-  moveItemInArray,
-  transferArrayItem
-} from '@angular/cdk/drag-drop';
-
-export interface Todo {
-  title: string;
-  date: string;
-}
-@Component({
-  selector: 'app-devList',
-  templateUrl: './devList.component.html',
-  styleUrls: ['./devList.component.scss']
-})
-export class DevListComponent implements OnInit {
-  value = '';
-
-public todo: Todo[] = [
-    { title: 'Wake up', date: new Date().toString() },
-    { title: 'Shopping', date: new Date().toString() },
-  ];
-
-public done: Todo[] = [
-    { title: 'Write a blog', date: new Date().toString() },
-    { title: 'Study Electron', date: new Date().toString() }
-  ];
-
-arr = [];
+  import { Component, OnInit } from '@angular/core';
+  import { Router } from '@angular/router';
+  import {
+    CdkDragDrop,
+    moveItemInArray,
+    transferArrayItem
+  } from '@angular/cdk/drag-drop';
 
 
-constructor(private router:Router) {}
-
-ngOnInit() {}
-
-onSubmit() {
-    this.todo.push({ title: this.value, date: new Date().toString() });
-    this.value = '';
+  export interface Todo {
+    firstname: string;
+    lastname: string;
+    picture: string;
   }
+  @Component({
+    selector: 'app-devList',
+    templateUrl: './devList.component.html',
+    styleUrls: ['./devList.component.scss']
+  })
+  export class DevListComponent implements OnInit {
 
-clearInput(event: MouseEvent) {
-    if ((<HTMLElement>event.target).nodeName === 'MAT-ICON') {
+  public todo: Todo[] = [
+      {
+        "firstname": "Alexandre",
+        "lastname": "Chartrand",
+        "picture": ""
+      },
+      {
+        "firstname": "Mathieu",
+        "lastname": "Perron",
+        "picture": ""
+      },
+      {
+        "firstname": "Élodie",
+        "lastname": "Bérubé",
+        "picture": ""
+      }
+    ];
+
+  public done: Todo[] = [
+    {
+      "firstname": "Bob",
+      "lastname": "Terrier",
+      "picture": ""
+    },
+    {
+      "firstname": "Mathieu",
+      "lastname": "Olivier",
+      "picture": ""
+    },
+    {
+      "firstname": "Fabian",
+      "lastname": "Charest",
+      "picture": ""
+    }
+    ];
+
+
+  constructor(private router:Router) {}
+
+  ngOnInit() {}
+
+  /*
+  onSubmit() {
+      this.todo.push({ title: this.value, date: new Date().toString() });
       this.value = '';
     }
-  }
 
-deleteTask(index: number) {
-    this.done.splice(index, 1);
-  }
+    */
 
-onAreaListControlChanged(index: number) {
-    setTimeout(() => {
-      const task = this.todo.splice(index, 1);
-      this.done.unshift(task[0]);
-    }, 1000);
-  }
+  clearInput(event: MouseEvent) {
+      if ((<HTMLElement>event.target).nodeName === 'MAT-ICON') {
+        this.value = '';
+      }
+    }
 
-  public onGoToEditYourTeamPage(): void {
-    this.router.navigate(['editTeam']);
-  }
+  deleteTask(index: number) {
+      this.done.splice(index, 1);
+    }
 
-  public onGoToRandomChooserPage(): void {
-    this.router.navigate(['']);
-  }
+  onAreaListControlChanged(index: number) {
+      setTimeout(() => {
+        const task = this.todo.splice(index, 1);
+        this.done.unshift(task[0]);
+      }, 1000);
+    }
 
-drop(event: CdkDragDrop<string[]>, type: string) {
-    if (event.previousContainer !== event.container) {
-      transferArrayItem(event.previousContainer.data, event.container.data,
-        event.previousIndex, event.currentIndex);
-    } else {
-      if (type === 'todo') {
-        moveItemInArray(this.todo, event.previousIndex, event.currentIndex)
+    public onGoToEditYourTeamPage(): void {
+      this.router.navigate(['editTeam']);
+    }
+
+    public onGoToRandomChooserPage(): void {
+      this.router.navigate(['']);
+    }
+
+    drop(event: CdkDragDrop<string[]>) {
+      if (event.previousContainer === event.container) {
+        moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
       } else {
-        moveItemInArray(this.done, event.previousIndex, event.currentIndex);
+        transferArrayItem(event.previousContainer.data,
+                          event.container.data,
+                          event.previousIndex,
+                          event.currentIndex);
       }
     }
   }
-}
