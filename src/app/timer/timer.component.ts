81ee8d425cb3f9e1c539303c0a8e8cfba7e2bbe8
Alexandre Chartrand <chartrandalexandre@gmail.com>
2021-01-24 23:49:13 -0500

    Updating the spinner value each second.
    Updating the spinner value according to the time left not works.

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -2,6 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { config } from 'rxjs';
 
+// ngx-countdown does not have any onTick function, which could allow us to run a functon each second.
+// But it has a notify handled, which generate an event when the time left is contained in a list passed in parameter.
+// To simulate a notify each second, we generate a long list of integer (here 3600) and pass it to the notify configuration.
+// So, a notify event will be send each second for the last 3600 seconds left on the timer.
+function range(start, end) {
+  var list = [];
+  for (var i = start; i <= end; i++) {
+      list.push(i);
+  }
+  return list
+}
+const notifyList =  range(1,3600)
+
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
@@ -10,7 +23,7 @@ import { config } from 'rxjs';
 export class TimerComponent implements OnInit {
   
   timeToSet = {hour: 0, minute: 0, second: 0};
-  salut = {leftTime: 10, demand: true}
+  countdownConfig = {leftTime: 10, demand: true, notify: notifyList}
   spinnerValue = 100;
 
   
@@ -20,16 +33,18 @@ export class TimerComponent implements OnInit {
   }
 
   setTime(){
-    this.salut = 
+    this.countdownConfig = 
       {
         leftTime: this.timeToSet.hour*3600 
                    + this.timeToSet.minute*60
                    + this.timeToSet.second, 
-        demand: true}
+        demand: true,
+        notify: notifyList}
     this.spinnerValue = 100;
   }
 
- 
-  
-  
+  onTickUpdate(event){
+      // Updating the spinner value.
+      this.spinnerValue =  ((event.left/1000)/this.countdownConfig.leftTime)*100;
+  } 
 }
